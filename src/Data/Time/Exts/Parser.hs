b9572cd4131f4ff2c0cfdd5ce4e4855e92868904
Foo Bar <foo@bar.com>
2014-03-02 02:48:16 -0800

    time-exts-2.1.0: added new %-codes and parsers

--- src/Data/Time/Exts/Parser.hs
+++ src/Data/Time/Exts/Parser.hs
@@ -13,8 +13,21 @@
 -- | Timestamp parsers and related utilities.
 module Data.Time.Exts.Parser (
 
+ -- ** Parse Unix Timestamps
+       parseUnixDate
+     , parseUnixTime
+     , parseUnixTimeMillis
+     , parseUnixTimeMicros
+     , parseUnixTimeNanos
+     , parseUnixTimePicos
+     , parseUnixDateTime
+     , parseUnixDateTimeMillis
+     , parseUnixDateTimeMicros
+     , parseUnixDateTimeNanos
+     , parseUnixDateTimePicos
+
  -- ** Parse UTC and Local Timestamps
-       parseLocalDate
+     , parseLocalDate
      , parseLocalDateTime
      , parseLocalDateTimeMillis
      , parseLocalDateTimeMicros
@@ -29,17 +42,19 @@ module Data.Time.Exts.Parser (
 
 import Control.Applicative        ((<|>), (<$>), (*>))
 import Control.Exception          (Exception)
-import Control.Monad              (liftM)
+import Control.Monad              (liftM, liftM3)
 import Control.Monad.State.Strict (execState, State)
 import Data.Attoparsec.Text as P
 import Data.Convertible           (Convertible(..), prettyConvertError)
 import Data.Char                  (isAlpha, isDigit)
 import Data.Label                 ((:->), mkLabels)
 import Data.Label.Monadic         (puts, modify)
+import Data.List as L             (foldl1, map)
 import Data.String                (IsString(..))
 import Data.Text as T
 import Data.Time.Exts.Base hiding (TimeZone)
 import Data.Time.Exts.Local
+import Data.Time.Exts.Unix
 import Data.Time.Exts.Zone
 import Data.Typeable              (Typeable)
 
@@ -54,11 +69,92 @@ instance IsString ParseError where
 
 mkLabels [''DateTimeZoneStruct]
 
+-- | Parse a Unix date.
+--
+-- > >>> parseUnixDate "%F" "2014-03-02"
+-- > Right 2014-03-02
+--
+parseUnixDate :: FormatText -> Text -> Either ParseError UnixDate
+parseUnixDate format text = fun <$> parseStruct Universal format text
+  where fun DateTimeZoneStruct{..} = createUnixDate _dtz_year _dtz_mon _dtz_mday
+
+-- | Parse a Unix time.
+--
+-- > >>> parseUnixTime "%T" "02:28:32"
+-- > Right 02:28:32
+--
+parseUnixTime :: FormatText -> Text -> Either ParseError UnixTime
+parseUnixTime format text = fun <$> parseStruct Universal format text
+  where fun DateTimeZoneStruct{..} = createUnixTime _dtz_hour _dtz_min $ truncate _dtz_sec
+
+-- | Parse a Unix time with millisecond granularity.
+parseUnixTimeMillis :: FormatText -> Text -> Either ParseError UnixTimeMillis
+parseUnixTimeMillis format text = fun <$> parseStruct Universal format text
+  where fun DateTimeZoneStruct{..} =
+          let (sec, mil) = properFracMillis _dtz_sec
+          in createUnixTimeMillis _dtz_hour _dtz_min sec mil
+
+-- | Parse a Unix time with microsecond granularity.
+parseUnixTimeMicros :: FormatText -> Text -> Either ParseError UnixTimeMicros
+parseUnixTimeMicros format text = fun <$> parseStruct Universal format text
+  where fun DateTimeZoneStruct{..} =
+          let (sec, mic) = properFracMicros _dtz_sec
+          in createUnixTimeMicros _dtz_hour _dtz_min sec mic
+
+-- | Parse a Unix time with nanosecond granularity.
+parseUnixTimeNanos :: FormatText -> Text -> Either ParseError UnixTimeNanos
+parseUnixTimeNanos format text = fun <$> parseStruct Universal format text
+  where fun DateTimeZoneStruct{..} =
+          let (sec, nan) = properFracNanos _dtz_sec
+          in createUnixTimeNanos _dtz_hour _dtz_min sec nan
+
+-- | Parse a Unix time with picosecond granularity.
+parseUnixTimePicos :: FormatText -> Text -> Either ParseError UnixTimePicos
+parseUnixTimePicos format text = fun <$> parseStruct Universal format text
+  where fun DateTimeZoneStruct{..} =
+          let (sec, pic) = properFracPicos _dtz_sec
+          in createUnixTimePicos _dtz_hour _dtz_min sec pic
+
+-- | Parse a Unix date and time.
+--
+-- > >>> parseUnixDateTime "%FT%TZ" "2014-02-27T11:31:20Z"
+-- > Right 2014-02-27 11:31:20
+--
+parseUnixDateTime :: FormatText -> Text -> Either ParseError UnixDateTime
+parseUnixDateTime format text = fun <$> parseStruct Universal format text
+  where fun DateTimeZoneStruct{..} =
+          let sec = truncate _dtz_sec
+          in createUnixDateTime _dtz_year _dtz_mon _dtz_mday _dtz_hour _dtz_min sec
+
+-- | Parse a Unix date and time with millisecond granularity.
+parseUnixDateTimeMillis :: FormatText -> Text -> Either ParseError UnixDateTimeMillis
+parseUnixDateTimeMillis format text = fun <$> parseStruct Universal format text
+  where fun DateTimeZoneStruct{..} =
+          let (sec, mil) = properFracMillis _dtz_sec
+          in createUnixDateTimeMillis _dtz_year _dtz_mon _dtz_mday _dtz_hour _dtz_min sec mil
+
+-- | Parse a Unix date and time with microsecond granularity.
+parseUnixDateTimeMicros :: FormatText -> Text -> Either ParseError UnixDateTimeMicros
+parseUnixDateTimeMicros format text = fun <$> parseStruct Universal format text
+  where fun DateTimeZoneStruct{..} =
+          let (sec, mic) = properFracMicros _dtz_sec
+          in createUnixDateTimeMicros _dtz_year _dtz_mon _dtz_mday _dtz_hour _dtz_min sec mic
+
+-- | Parse a Unix date and time with nanosecond granularity.
+parseUnixDateTimeNanos :: FormatText -> Text -> Either ParseError UnixDateTimeNanos
+parseUnixDateTimeNanos format text = fun <$> parseStruct Universal format text
+  where fun DateTimeZoneStruct{..} =
+          let (sec, nan) = properFracNanos _dtz_sec
+          in createUnixDateTimeNanos _dtz_year _dtz_mon _dtz_mday _dtz_hour _dtz_min sec nan
+
+-- | Parse a Unix date and time with picosecond granularity.
+parseUnixDateTimePicos :: FormatText -> Text -> Either ParseError UnixDateTimePicos
+parseUnixDateTimePicos format text = fun <$> parseStruct Universal format text
+  where fun DateTimeZoneStruct{..} =
+          let (sec, pic) = properFracPicos _dtz_sec
+          in createUnixDateTimePicos _dtz_year _dtz_mon _dtz_mday _dtz_hour _dtz_min sec pic
+
 -- | Parse a local date.
---
--- > >>> parseLocalDate Seoul "%Y년 %m월 %d일 (%Z)" "2009년 03월 10일 (KST)"
--- > Right 2009-03-10 KST
---
 parseLocalDate :: City -> FormatText -> Text -> Either ParseError LocalDate
 parseLocalDate city format text = convert <$> parseStruct city format text
   where convert DateTimeZoneStruct{..} = createLocalDate _dtz_year _dtz_mon _dtz_mday _dtz_zone
@@ -119,25 +215,32 @@ parseFormat city format =
 
 -- | Create a format string parser.
 createParser :: City -> Parser (Parser (Either ParseError (State DateTimeZoneStruct ())))
-createParser city = 
+createParser x = 
       matchLit
-  <|> matchSet "%Y" dtz_year (parseFixedInt 4)
-  <|> matchSet "%m" dtz_mon  (parseMonth)
+  <|> matchSet "%a" dtz_wday  parseWeekShort
+  <|> matchSet "%b" dtz_mon   parseMonthShort
   <|> matchSet "%d" dtz_mday (parseFixedInt 2)
+  <|> matchSet "%h" dtz_mon   parseMonthShort
+  <|> matchSet "%m" dtz_mon  (parseMonthInt 2)
+  <|> matchMod "%p" dtz_hour  parsePeriodLow
+  <|> matchSet "%A" dtz_wday  parseWeekLong
+  <|> matchSet "%B" dtz_mon   parseMonthLong
+  <|> matchYMD "%F" dtz_year  dtz_mon dtz_mday
   <|> matchSet "%H" dtz_hour (parseFixedInt 2)
   <|> matchSet "%M" dtz_min  (parseFixedInt 2)
-  <|> matchSet "%S" dtz_sec  (parseSecond)
-  <|> matchSet "%Z" dtz_zone (parseTimeZone city)
-  <|> matchMod "%p" dtz_hour (parsePeriodLow)
-  <|> matchMod "%P" dtz_hour (parsePeriodHigh)
+  <|> matchMod "%P" dtz_hour  parsePeriodHigh
+  <|> matchSet "%S" dtz_sec   parseSecond
+  <|> matchHMS "%T" dtz_hour  dtz_min dtz_sec
+  <|> matchSet "%Y" dtz_year (parseFixedInt 4)
+  <|> matchSet "%Z" dtz_zone (parseTimeZone x)
   <|> matchAny
 
 -- | Match a percent literal.
 matchLit :: Parser (Parser (Either ParseError (State DateTimeZoneStruct ())))
 matchLit = string "%%" *> return (char '%' *> return (return (return ())))
 
--- | Match a percent code and set the state
---   using the value returned by the parser.
+-- | Match a percent code and update the field
+--   state with the value returned by the parser.
 matchSet 
   :: Text
   -> (DateTimeZoneStruct :-> a)
@@ -146,35 +249,114 @@ matchSet
 matchSet code field parser =
   string code *> return (puts field <$$> parser)
 
--- | Match a percent code and modify the state
---   using the function returned by the parser.
-matchMod 
+-- | Match a year-month-day percent code and update the
+--   field states with the values returned by the parser.
+matchYMD
+  :: Text
+  -> (DateTimeZoneStruct :-> Year )
+  -> (DateTimeZoneStruct :-> Month)
+  -> (DateTimeZoneStruct :-> Day  )
+  -> Parser (Parser (Either ParseError (State DateTimeZoneStruct ())))
+matchYMD code year mon day = string code *> return (fields <$$> parser)
+  where fields (y,m,d) = puts year y *> puts mon m *> puts day d
+        parser = do
+          y <- parseFixedInt 4
+          _ <- char '-'
+          m <- parseMonthInt 2
+          _ <- char '-'
+          d <- parseFixedInt 2
+          return $! liftM3 (,,) y m d
+
+-- | Match a hour-minute-second percent code and update
+--   the field states with the values returned by the parser.
+matchHMS
+  :: Text
+  -> (DateTimeZoneStruct :-> Hour  )
+  -> (DateTimeZoneStruct :-> Minute)
+  -> (DateTimeZoneStruct :-> Double)
+  -> Parser (Parser (Either ParseError (State DateTimeZoneStruct ())))
+matchHMS code hour mn sec = string code *> return (fields <$$> parser)
+  where fields (h,m,s) = puts hour h *> puts mn m *> puts sec s
+        parser = do
+          h <- parseFixedInt 2
+          _ <- char ':'
+          m <- parseFixedInt 2
+          _ <- char ':'
+          s <- parseSecond
+          return $! liftM3 (,,) h m s
+
+-- | Match a percent code and modify the field
+--   state with the function returned by the parser.
+matchMod
   :: Text
   -> (DateTimeZoneStruct :-> a)
   -> Parser (Either ParseError (a -> a))
   -> Parser (Parser (Either ParseError (State DateTimeZoneStruct ())))
-matchMod code field parser = 
+matchMod code field parser =
   string code *> return (modify field <$$> parser)
 
--- | Match any character sequence.
+-- | Match any other character sequence.
 matchAny :: Parser (Parser (Either ParseError (State DateTimeZoneStruct ())))
-matchAny = takeWhile1 (/='%') >>= return . \ text -> const (return ()) <$$> do
-  str <- liftM toLower . P.take $ T.length text
-  return $! if str == toLower text then Right str else Left "matchAny: mismatch"
+matchAny  = takeWhile1 (/='%') >>= return . \ text -> const (return ()) <$$> do
+  let src = toLower text
+  trg <- liftM toLower . P.take $ T.length src
+  return $! if src == trg
+    then Right src
+    else let msg = "matchAny: " ++ show src ++ " not equal to " ++ show trg
+         in  Left $ ParseError msg
 
--- | Parse an integral type with fixed length.
+-- | Parse an integer with fixed length.
 parseFixedInt :: Integral a => Int -> Parser (Either ParseError a)
-parseFixedInt n = either (Left . ParseError) Right . parseOnly decimal <$> P.take n
+parseFixedInt n = do
+  text <- P.take n
+  return $! if not $ T.all isDigit text
+    then Left  . ParseError   $ "parseFixedInt: " ++ unpack text
+    else Right . fromIntegral $ T.foldl s2n 0 text
+    where s2n a b = a * 10 + fromEnum b - 48
 
--- | Parse a month.
-parseMonth :: Parser (Either ParseError Month)
-parseMonth  = fun <$> parseFixedInt 2 where
-  fun = \ case
-    Right n | 1 <= n && n <= 12 -> Right $ toEnum (n-1)
-    Right n -> Left . ParseError $ "parseMonth: " ++ show n
+-- | Parse a month in integer format with fixed length.
+parseMonthInt :: Int -> Parser (Either ParseError Month)
+parseMonthInt n = cases <$> parseFixedInt n where
+  cases = \ case
+    Right v | 1 <= v && v <= 12 -> Right $ toEnum (v-1)
+    Right v -> Left . ParseError $ "parseMonthInt: " ++ show v
     Left  e -> Left e
 
--- | Parse a second.
+-- | Parse a month in short text format.
+parseMonthShort :: Parser (Either ParseError Month)
+parseMonthShort = parseText list <|> left
+  where left = return (Left $ ParseError "parseMonthShort: not enough input")
+        list = [("Jan", January),("Feb", February),("Mar", March    )
+               ,("Apr", April  ),("May", May     ),("Jun", June     )
+               ,("Jul", July   ),("Aug", August  ),("Sep", September)
+               ,("Oct", October),("Nov", November),("Dec", December )]
+
+-- | Parse a month in long text format.
+parseMonthLong :: Parser (Either ParseError Month)
+parseMonthLong = parseText list <|> left
+  where left = return (Left $ ParseError "parseMonthLong: not enough input")
+        list = [("January", January),("February", February),("March"    , March    )
+               ,("April"  , April  ),("May"     , May     ),("June"     , June     )
+               ,("July"   , July   ),("August"  , August  ),("September", September)
+               ,("October", October),("November", November),("December" , December )]
+
+-- | Parse a day of week in short text form. 
+parseWeekShort :: Parser (Either ParseError DayOfWeek)
+parseWeekShort = parseText list <|> left
+  where left = return (Left $ ParseError "parseWeekShort: not enough input")
+        list = [("Sun", Sunday   ),("Mon", Monday  ),("Tue", Tuesday)
+               ,("Wed", Wednesday),("Thu", Thursday),("Fri", Friday )
+               ,("Sat", Saturday )]
+
+-- | Parse a day of week in long text form. 
+parseWeekLong :: Parser (Either ParseError DayOfWeek)
+parseWeekLong = parseText list <|> left
+  where left = return (Left $ ParseError "parseWeekShort: not enough input")
+        list = [("Sunday"   , Sunday   ),("Monday"  , Monday  ),("Tuesday", Tuesday)
+               ,("Wednesday", Wednesday),("Thursday", Thursday),("Friday" , Friday )
+               ,("Saturday" , Saturday )]
+
+-- | Parse a second with arbitrary precision.
 parseSecond :: Parser (Either ParseError Double)
 parseSecond = do
   text1 <- P.take 2
@@ -188,14 +370,6 @@ parseSecond = do
              z = realToFrac $ T.length text2
          return $! Right $ x + y * 10**(-z)
          where s2n a b = a * 10 + fromIntegral (fromEnum b) - 48 :: Int
-
--- | Parse a time zone.
-parseTimeZone :: City -> Parser (Either ParseError TimeZone)
-parseTimeZone city = do
-  text <- takeWhile1 isAlpha
-  let zone = safeConvert . TimeZoneAbbr city $ unpack text
-  return $! either err Right zone
-  where err = Left . ParseError . prettyConvertError
 
 -- | Parse lowercase am//pm symbols.
 parsePeriodLow :: Parser (Either ParseError (Hour -> Hour))
@@ -212,6 +386,19 @@ parsePeriodHigh = period <$> P.take 2
             "AM" -> Right $ \ case 12 -> 00; x -> x
             "PM" -> Right $ \ case 12 -> 12; x -> x + 12
             text -> Left  . ParseError $ "parsePeriodHigh: " ++ unpack text
+
+-- | Parse a time zone.
+parseTimeZone :: City -> Parser (Either ParseError TimeZone)
+parseTimeZone city = do
+  text <- takeWhile1 isAlpha
+  let zone = safeConvert . TimeZoneAbbr city $ unpack text
+  return $! either err Right zone
+  where err = Left . ParseError . prettyConvertError
+
+-- | Parse text and match with value.
+parseText :: [(Text, a)] -> Parser (Either ParseError a)
+parseText = L.foldl1 (<|>) . L.map match
+  where match (name, val) = asciiCI name *> return (Right val)
 
 -- | Twice promote a function to a monad.
 (<$$>) :: (Functor f1, Functor f2) => (a -> b) -> f1 (f2 a) -> f1 (f2 b)
