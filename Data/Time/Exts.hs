1a83acb12ba1b448a10bc153ef065f800b06ed6f
Enzo Haussecker <enzo@sovereign.io>
2016-11-25 13:49:00 -0800

    time-exts-3.0.0: expose core modules

---
+++ Data/Time/Exts.hs
@@ -0,0 +1,20 @@
+-- |
+-- Module     : Data.Time.Exts
+-- Copyright  : 2013-2016 Enzo Haussecker
+-- License    : BSD3
+-- Maintainer : Enzo Haussecker <enzo@sovereign.io>
+-- Stability  : Stable
+
+module Data.Time.Exts (
+
+       module Data.Time.Exts.Base
+     , module Data.Time.Exts.Format
+     , module Data.Time.Exts.UTC
+     , module Data.Time.Exts.Unix
+
+     ) where
+
+import Data.Time.Exts.Base
+import Data.Time.Exts.Format
+import Data.Time.Exts.UTC
+import Data.Time.Exts.Unix
